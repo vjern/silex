88c27f5fa1add40b3e79934d54e61193d3ff1fd7
zebralt <jocelyn.vernay@ipsos.com>
2021-10-14 18:13:42 +0200

    Add more tests to tree.py

--- tree.py
+++ tree.py
@@ -2,7 +2,15 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import Any, Dict, List, Literal, Set, Tuple, Union
 
-from g import Symbols
+
+class Symbols(Enum):
+    Ident = 'ident'
+    LeftPar, RightPar = '(', ')'
+    LeftBracket, RightBracket = '{', '}'
+    LeftSquareBracket, RightSquareBracket = '[', ']'
+    FuncKeyword = 'fn'
+    Comma = ','
+    NumberLiteral = r'\d+'
 
 
 class Specials(Enum):
@@ -16,9 +24,11 @@ from unittest.mock import Mock
 N = Mock(goal=None)
 
 
+# FIXME add support for Empty and multiple children for same link
+
 @dataclass
 class Node:
-    goal: Any = None
+    goal: Any = None  # if provided, should evaluate to True
     leaves: Set[Any] = field(default_factory=set)
     children: Dict[Union[Enum, Specials, None], 'Node'] = field(default_factory=dict)
 
@@ -31,8 +41,8 @@ class Node:
         if not items:
             return (False, 0)
         first = items[0]
-        if (take_first or not items[1:]) and (self.children.get(first, N).goal or self.children.get(Specials.Any, N).goal):
-            return (True, 1)
+        if (take_first or not items[1:]) and (goal := (self.children.get(first, N).goal or self.children.get(Specials.Any, N).goal)):
+            return (goal, 1)
         t = self.children.get(first)
         if t:
             ans, cskip = t.find(items[1:], take_first=take_first)
@@ -43,7 +53,16 @@ class Node:
             if cskip:
                 ans = cans
             skip += cskip
+        elif Specials.Empty in self.children:
+            ans = take_first
+            cans, cskip = self.children[Specials.Empty].find(items, take_first=take_first)
+            if cskip:
+                ans = cans
+            skip += cskip
         return (ans, skip)
+    
+    def __or__(self, node):
+        return Node(children={Specials.Empty: [self, node]})
 
 
 class Rshifter:
@@ -112,16 +131,48 @@ class TestNode:
         }
 
         assert end.children == {}
-
-    class TestFind:
+    
+    def test_concrete_oneliner(self):
 
         start = Node()
         end = Node(goal=True)
         item = Node()
 
-        start >> Symbols.LeftPar >> item
-        item  >> [Specials.Any, Symbols.Comma] >> item
-        item  >> Symbols.RightPar >> end
+        (
+            start >> Symbols.LeftPar >> 
+            item >> [Specials.Any, Symbols.Comma] >>
+            item >> Symbols.RightPar >> end
+        )
+        
+        assert start.children == {
+            Symbols.LeftPar: item,
+        }
+
+        assert item.children == {
+            Specials.Any: Node(children={
+                Symbols.Comma: item
+            }),
+            Symbols.RightPar: end
+        }
+
+        assert end.children == {}
+
+    class TestFind:
+
+        class Expr:
+
+            @classmethod
+            def automaton(cls):
+
+                start = Node()
+                end = Node(goal=cls)
+                item = Node()
+
+                start >> Symbols.LeftPar >> item
+                item  >> [Specials.Any, Symbols.Comma] >> item
+                item  >> Symbols.RightPar >> end
+
+                return start
 
         def test_basic(self):
             
@@ -129,7 +180,7 @@ class TestNode:
                 Symbols.LeftPar,
                 Symbols.RightPar
             ]
-            assert self.start.find(units) == (True, 2)
+            assert self.Expr.automaton().find(units) == (self.Expr, 2)
         
         def test_extended(self):
     
@@ -138,7 +189,7 @@ class TestNode:
                 Symbols.Ident,
                 Symbols.RightPar
             ]
-            assert self.start.find(units) == (False, 3)
+            assert self.Expr.automaton().find(units) == (False, 3)
 
             units = [
                 Symbols.LeftPar,
@@ -146,7 +197,7 @@ class TestNode:
                 Symbols.Comma,
                 Symbols.RightPar
             ]
-            assert self.start.find(units) == (True, 4)
+            assert self.Expr.automaton().find(units) == (self.Expr, 4)
     
             units = [
                 Symbols.LeftPar,
@@ -156,5 +207,44 @@ class TestNode:
                 Symbols.Comma,
                 Symbols.RightPar
             ]
-            assert self.start.find(units) == (True, 6)
+            assert self.Expr.automaton().find(units) == (self.Expr, 6)
     
+    class TestAmbiguous:
+
+        def test_ambiguous(self):
+
+            # ( Symbols.Ident )
+            class parexpr:
+                start = Node()
+                ( start
+                >> [ Symbols.LeftPar, Symbols.Ident, Symbols.RightPar ] >> Node(goal='parexpr')
+                )
+
+            # ( Symbols.Ident, )
+            class parlist:
+                start = Node()
+                item  = Node()
+                ( start
+                >> Symbols.LeftPar >> item
+                    >> [ Symbols.Ident, Symbols.Comma ] >> item
+                        >> Symbols.RightPar >> Node(goal='parlist')
+                    >> Node(goal='parexpr')
+                )
+
+            units_parexpr = [
+                Symbols.LeftPar, Symbols.Ident, Symbols.RightPar
+            ]
+
+            assert parexpr.start.find(units_parexpr) == ('parexpr', 3)
+
+            units_parlist = [
+                Symbols.LeftPar, Symbols.Ident, Symbols.Comma, Symbols.RightPar
+            ]
+
+            assert parlist.start.find(units_parlist) == ('parlist', 4)
+
+            assert parexpr.start.find(units_parlist) == (False, 3)
+            assert parlist.start.find(units_parexpr) == (False, 3)
+
+            # assert (parexpr.start | parlist.start).find(units_parlist) == ('parlist', 4)
+            # assert (parexpr.start | parlist.start).find(units_parexpr) == ('parexpr', 3)
